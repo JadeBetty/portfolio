0a9049ed816c36d410c382d65a2771e61222ed43
JadeBetty <jadebetty.7261@gmail.com>
2024-01-20 21:47:24 +0700

    small little update

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,9 +4,15 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-	fontFamily: {
-		'sans': ['SanFrancisco', 'san-serif']
-	}
+    fontFamily: {
+      sans: ["Inter", "san-serif"],
+    },
+    extend: {
+      colors: {
+        "background": "#080904",
+        "text": "#EDEFE9"
+      }
+    }
   },
   plugins: [],
 };
