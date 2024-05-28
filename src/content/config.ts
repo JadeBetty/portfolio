5bfdf0333ba56b2eb0f26468ca4ce1791bf2c4a8
JadeBetty <contact@jadebetty.me>
2024-05-28 15:32:23 +0700

    jade steal code

---
+++ src/content/config.ts
@@ -0,0 +1,18 @@
+//  !!! THIS IS NOT MY CODE 
+
+
+import { z, defineCollection } from 'astro:content';
+
+const blogCollection = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    pubDate: z.string(),
+    draft: z.boolean().optional(),
+    readingTime: z.string().optional(),
+  }),
+});
+
+export const collections = {
+  'blog': blogCollection,
+};
