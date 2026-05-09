6719160216266c8833c6805af1a15e85756b4f1d
JadeBetty <jadebetty.7261@gmail.com>
2026-05-09 08:24:17 +0700

    feat: added more links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,6 +13,8 @@ export default function Home() {
       name: "Musixmatch",
       href: "https://www.musixmatch.com/profile/3vUCAMIiJQmjDoTq78_w0ZXfwkSRYiSbWdCBtOUXB4Q9fYEJNN1pxvW7MGTO2AJ0wo3pL4eLm7Cs9PYYMffryJ5ntos_pH-dhwk9d1WCG9Nv7bgsUPQH8RPJPohnlKboeseX8vrZAP2icbx-oMO-wrR_Txs",
     },
+    { name: "Letterboxd", href: "https://boxd.it/eNW6r" },
+    { name: "Serializd", href: "https://srlzd.com/u/JadeBetty" },
     { name: "Email", href: "mailto:contact@jadebetty.tech" },
   ];
 
